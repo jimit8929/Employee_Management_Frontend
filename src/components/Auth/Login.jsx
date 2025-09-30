8296b1e23299ea6c3f54345c92dacaf43de7a6d5
jimit8929 <jimit8929@gmail.com>
2025-09-30 19:44:31 +0530

    Login Page

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className="flex items-center justify-center h-screen bg-black w-screen">
+      <div className="border-2 border-red-600">
+        <form className="flex flex-col items-center justify-center">
+          <input type="email" placeholder="Enter your Email" className="border-2 border-emerald-600 rounded-full py-4 px-5 text-xl outline-none bg-transparent placeholder:text-white"/>
+          <input type="password" placeholder="Enter your Password" className="border-2 border-emerald-600 rounded-full py-4 px-5 text-xl outline-none bg-transparent placeholder:text-white"/>
 
-export default Login
+          <button>Login</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
